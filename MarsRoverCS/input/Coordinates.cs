637ddc6e9f17eb33bec2ba069a15d6c2114abe4b
AbiPetheram <abi.gill97@gmail.com>
2024-10-16 17:59:20 +0100

    feat: add coordinate record

---
+++ MarsRoverCS/input/Coordinates.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject.input
+{
+    internal record Coordinates(int x, int y);
+
+}
