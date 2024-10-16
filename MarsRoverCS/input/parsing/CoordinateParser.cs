74c812f3629fc4078f9398abef5f4e296188e0d4
AbiPetheram <abi.gill97@gmail.com>
2024-10-16 17:59:41 +0100

    feat: implement coordinate parser

---
+++ MarsRoverCS/input/parsing/CoordinateParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject.input.parsing
+{
+    internal class CoordinateParser
+    {
+        public Coordinates parseCoordinates(string[] input)
+        {
+            if(input == null || input.Length != 2)
+            {
+                throw new ArgumentOutOfRangeException();
+            } else if (!char.IsDigit(input[0][0]) || !char.IsDigit(input[1][0]))
+            {
+                throw new ArgumentOutOfRangeException();
+            } else
+            {
+                return new Coordinates(Int32.Parse(input[0]), Int32.Parse(input[1]));
+            }
+        }
+    }
+}
