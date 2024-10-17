35769ac6142e1756212f56226730baaea2092700
AbiPetheram <abi.gill97@gmail.com>
2024-10-17 16:15:00 +0100

    refactor: update namespace

--- MarsRoverCS/input/parsing/CoordinateParser.cs
+++ MarsRoverCS/input/parsing/CoordinateParser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstProject.input.parsing
+namespace MarsRoverCS.input.parsing
 {
     public class CoordinateParser
     {
