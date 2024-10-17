2d173d13564840ffa4563d9bf3412d81d5eea708
AbiPetheram <abi.gill97@gmail.com>
2024-10-17 16:14:41 +0100

    test: add test for lower case valid input direction parser

--- MarsRoverCSTest/input/parsing/DirectionParserTests.cs
+++ MarsRoverCSTest/input/parsing/DirectionParserTests.cs
@@ -40,8 +40,45 @@ namespace MarsRoverCS.input.parsing.Tests
         public void TestNorthDirection()
         {
             var parser = new DirectionParser();
+            var result = parser.ParseDirection("N");
+            Assert.AreEqual(CompassDirection.N, result);
+        }
+
+        [TestMethod()]
+        public void TestEastDirection()
+        {
+            var parser = new DirectionParser();
             var result = parser.ParseDirection("E");
             Assert.AreEqual(CompassDirection.E, result);
         }
+
+        [TestMethod()]
+        public void TestSouthDirection()
+        {
+            var parser = new DirectionParser();
+            var result = parser.ParseDirection("S");
+            Assert.AreEqual(CompassDirection.S, result);
+        }
+
+
+        [TestMethod()]
+        public void TestWestDirection()
+        {
+            var parser = new DirectionParser();
+            var result = parser.ParseDirection("W");
+            Assert.AreEqual(CompassDirection.W, result);
+        }
+
+        [TestMethod()]
+        public void TestLowerCaseValidDirection()
+        {
+            var parser = new DirectionParser();
+   
+            Assert.AreEqual(CompassDirection.W, parser.ParseDirection("w"));
+            Assert.AreEqual(CompassDirection.E, parser.ParseDirection("e"));
+            Assert.AreEqual(CompassDirection.N, parser.ParseDirection("n"));
+            Assert.AreEqual(CompassDirection.S, parser.ParseDirection("s"));
+        }
+
     }
 }
