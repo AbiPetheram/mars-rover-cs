e88e8521899677691b0b27eada2c89b763646097
AbiPetheram <abi.gill97@gmail.com>
2024-10-17 15:15:41 +0100

    test: add test for null coordinates

---
+++ MarsRoverCSTest/input/parsing/CoordinateParserTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstProject.input.parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject.input.parsing.Tests
+{
+    [TestClass()]
+    public class CoordinateParserTests
+    {
+        [TestMethod()]
+        public void TestNullCoordinateThrowsIllegalArgumentException()
+        {
+            var parser = new CoordinateParser();
+            Assert.ThrowsException<ArgumentException>(
+                () => parser.ParseCoordinates(null));
+        }
+    }
+}
