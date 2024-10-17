2ba6183d8ddcb0224129ba961b63b0ca6312bfc5
AbiPetheram <abi.gill97@gmail.com>
2024-10-17 14:27:58 +0100

    feat: implement ParseInstructions

---
+++ MarsRoverCS/input/parsing/InstructionParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.input.parsing
+{
+    internal class InstructionParser
+    {
+        public Instruction[] ParseInstructions(String input)
+        {
+            if(String.IsNullOrEmpty(input)) throw new ArgumentNullException(nameof(input));
+            Instruction[] result = new Instruction[input.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                switch (char.ToUpper(input[i]))
+                {
+                    case 'L':
+                        result[i] = Instruction.L;
+                        break;
+                    case 'R':
+                        result[i] = Instruction.R;
+                        break;
+                    case 'M':
+                        result[i] = Instruction.M;
+                        break;
+                    default:
+                        throw new ArgumentException(nameof(input));
+                }
+            }
+            return result;
+        }
+    }
+}
