fb7055095fa9ee60de3eab2e9c297e32e6e37bd0
AbiPetheram <abi.gill97@gmail.com>
2024-10-17 14:12:28 +0100

    feat: implement CompassDirection enum

---
+++ MarsRoverCS/input/CompassDirection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.input
+{
+    internal enum CompassDirection
+    {
+        N,
+        E,
+        S,
+        W
+    }
+}
