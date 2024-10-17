3fd0b2adf2151f2e9a2388a4c8a02bb92d42cc9e
AbiPetheram <abi.gill97@gmail.com>
2024-10-17 14:12:37 +0100

    feat: implement DirectionParser

---
+++ MarsRoverCS/input/parsing/DirectionParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.input.parsing
+{
+    internal class DirectionParser
+    {
+        public CompassDirection ParseDirection(string input)
+        {
+            if(input.Length > 0 || !char.IsLetter(input[0]))
+            {
+                throw new ArgumentOutOfRangeException(nameof(input));
+            }
+            return input.ToUpper() switch
+            {
+                "N" => CompassDirection.N,
+                "S" => CompassDirection.S,
+                "E" => CompassDirection.E,
+                "W" => CompassDirection.W,
+                _ => throw new ArgumentException()
+            };
+        }
+    }
+}
