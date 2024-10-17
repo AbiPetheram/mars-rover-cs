211246713f7f31a01a24904ecddc13aaab1bebc5
AbiPetheram <abi.gill97@gmail.com>
2024-10-17 16:39:57 +0100

    feat: implement position class with properties and overridden ToString method

---
+++ MarsRoverCS/input/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.input
+{
+    public class Position
+    {
+        private Coordinates coordinates { get; set;}
+        private CompassDirection direction { get; set;} 
+
+        override
+        public string ToString()
+        {
+            return coordinates.x + " " + coordinates.y + " " + direction.ToString();
+        }
+    }
+}
