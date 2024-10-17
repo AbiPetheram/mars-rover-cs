0eb36ec59620fd7a51320e85405ae7a0aba706a1
AbiPetheram <abi.gill97@gmail.com>
2024-10-17 15:16:19 +0100

    test: create test classes for direction and instruction parser

---
+++ MarsRoverCSTest/input/parsing/DirectionParserTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarsRoverCS.input.parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.input.parsing.Tests
+{
+    [TestClass()]
+    public class DirectionParserTests
+    {
+        [TestMethod()]
+        public void ParseDirectionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ MarsRoverCSTest/input/parsing/InstructionParserTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarsRoverCS.input.parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.input.parsing.Tests
+{
+    [TestClass()]
+    public class InstructionParserTests
+    {
+        [TestMethod()]
+        public void ParseInstructionsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
