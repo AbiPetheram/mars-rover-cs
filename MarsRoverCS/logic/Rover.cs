c4d12286dc9487826f8523fdf262669a86a5bb84
AbiPetheram <abi.gill97@gmail.com>
2024-10-18 15:47:45 +0100

    feat: add Rover class

---
+++ MarsRoverCS/logic/Rover.cs
@@ -0,0 +1,21 @@
+﻿using MarsRoverCS.input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.logic
+{
+    public class Rover
+    {
+        private Position Position { get; set; }
+        private Plateau Plateau { get; set; }
+
+        public Rover(Position position, Plateau plateau)
+        {
+            Position = position;
+            Plateau = plateau;
+        }
+    }
+}
