ec2d1d8e39a7a744f083e050d89c68f17a1566ab
AbiPetheram <abi.gill97@gmail.com>
2024-10-22 14:55:39 +0100

    feat: implement MoveForward method

--- MarsRoverCS/logic/Rover.cs
+++ MarsRoverCS/logic/Rover.cs
@@ -9,13 +9,43 @@ namespace MarsRoverCS.logic
 {
     public class Rover
     {
-        private Position Position { get; set; }
-        private Plateau Plateau { get; set; }
+        public Position Position { get; set; }
+        public Plateau Plateau { get; set; }
 
         public Rover(Position position, Plateau plateau)
         {
             Position = position;
             Plateau = plateau;
+        }
+
+        public void move(Instruction[] instructions)
+        {
+            foreach (Instruction instruction in instructions)
+            {
+                if(instruction == Instruction.M)
+                {
+                    moveForward(instruction);
+                }
+            }
+        }
+
+        private void moveForward(Instruction instruction)
+        {
+            switch (Position.Direction)
+            {
+                case CompassDirection.N:
+                    Position = new Position(new Coordinates(Position.Coordinates.x, Position.Coordinates.y + 1), Position.Direction);
+                    break;
+                case CompassDirection.E:
+                    Position = new Position(new Coordinates(Position.Coordinates.x + 1, Position.Coordinates.y), Position.Direction);
+                    break;
+                case CompassDirection.S:
+                    Position = new Position(new Coordinates(Position.Coordinates.x, Position.Coordinates.y - 1), Position.Direction);
+                    break;
+                case CompassDirection.W:
+                    Position = new Position(new Coordinates(Position.Coordinates.x - 1, Position.Coordinates.y), Position.Direction);
+                    break;
+            }
         }
     }
 }
