e6958c5e1caade274081ea43b8112b9d4254a7da
AbiPetheram <abi.gill97@gmail.com>
2024-10-23 15:17:06 +0100

    feat: implement MissionControl class with CreatePlateau method

---
+++ MarsRoverCS/logic/MissionControl.cs
@@ -0,0 +1,30 @@
+﻿using MarsRoverCS.input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.logic
+{
+    public class MissionControl
+    {
+        public Dictionary<Plateau, List<Rover>> PlateauRovers;
+
+        public MissionControl()
+        {
+            this.PlateauRovers = new Dictionary<Plateau, List<Rover>>();
+        }
+
+        public Plateau CreatePlateau(Coordinates Coordinates)
+        {
+            if(Coordinates == null)
+            {
+                throw new ArgumentNullException(nameof(Coordinates));
+            }
+            Plateau plateau = new Plateau(Coordinates);
+            PlateauRovers.Add(plateau, new List<Rover>()); 
+            return plateau;
+        }
+    }
+}
