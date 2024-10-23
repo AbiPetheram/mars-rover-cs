b89e780d91961fec201c970b4c11988d3d958172
AbiPetheram <abi.gill97@gmail.com>
2024-10-23 15:16:43 +0100

    test: check create plateau throws exception withnull input

---
+++ MarsRoverCSTest/logic/MissionControlTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarsRoverCS.logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverCS.logic.Tests
+{
+    [TestClass()]
+    public class MissionControlTests
+    {
+
+        [TestMethod()]
+        public void CreatePlateauTestNullInput()
+        {
+            var MissionControl = new MissionControl();
+            Assert.ThrowsException<ArgumentNullException>(() => MissionControl.CreatePlateau(null));
+        }
+    }
+}
